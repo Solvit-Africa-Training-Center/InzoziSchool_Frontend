0def4f748ee2a0bd8c74750f2f2bb35cdebef87f
Philemon-ndayishimiye <philemonndayi@gmail.com>
2025-09-02 09:25:56 +0200

    fix conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,3 @@
-import ParentSection from './Components/ParentSection';
-import PowerfulFeaturesSection from './Components/PowerfulFeaturesSection';
-import ResourcesSupportSection from './Components/ResourcesSupportSection';
-import SchoolOwnerSection from './Components/SchoolOwnerSection';
-import WhyChooseInzoziSection from './Components/WhyChooseInzoziSection';
 
 function App() {
   return (
