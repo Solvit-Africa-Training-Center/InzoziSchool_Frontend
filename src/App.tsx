33a16e6a6ac7eee66861cbe508cc6233e3b58d8d
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-09-02 09:21:28 +0200

    fixing app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import ParentSection from './Components/ParentSection';
-import PowerfulFeaturesSection from './Components/PowerfulFeaturesSection';
-import ResourcesSupportSection from './Components/ResourcesSupportSection';
-import SchoolOwnerSection from './Components/SchoolOwnerSection';
-import WhyChooseInzoziSection from './Components/WhyChooseInzoziSection';
+
 
 function App() {
   return (
     <>
-      <WhyChooseInzoziSection />
-      <ParentSection />
-      <PowerfulFeaturesSection/>
-      <SchoolOwnerSection />
-      <ResourcesSupportSection />
+     
     </>
   );
 }
