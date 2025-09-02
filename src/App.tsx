f31d92305111c3b297054325f7985f35c456e1bc
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-09-02 09:43:56 +0200

    resolved app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+<<<<<<< HEAD
 
 function App() {
   return (
    <>
    </>
+=======
+import ParentSection from './Components/ParentSection';
+import PowerfulFeaturesSection from './Components/PowerfulFeaturesSection';
+import ResourcesSupportSection from './Components/ResourcesSupportSection';
+import SchoolOwnerSection from './Components/SchoolOwnerSection';
+import WhyChooseInzoziSection from './Components/WhyChooseInzoziSection';
+
+function App() {
+  return (
+    <>
+      <WhyChooseInzoziSection />
+      <ParentSection />
+      <PowerfulFeaturesSection/>
+      <SchoolOwnerSection />
+      <ResourcesSupportSection />
+    </>
+>>>>>>> abd1a8a ( resolved app)
   );
 }
 
